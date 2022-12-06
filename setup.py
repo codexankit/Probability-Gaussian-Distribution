8b071f89437e731041b19e879620b55ac680ffc9
codexankit <108314952+codexankit@users.noreply.github.com>
2022-12-06 19:42:37 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='demogorgan_probability',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['demogorgan_probability'],
+      author='Ankit Kumar Prem',
+      author_email='ankitkumarprem@gmail.com',
+      zip_safe=False)
